a00389d029df7c51d1bae0b606e6dd812a48733b
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-02-09 11:42:03 +0100

    dev: added go back

--- front/PhotoBook/src/screens/Legal.tsx
+++ front/PhotoBook/src/screens/Legal.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-const Legal = () => {
+const Legal = ({navigation}: any) => {
   const name = 'Legal';
   return (
     <View style={styles.view}>
       <Text style={styles.text}>{name} works !</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 };
