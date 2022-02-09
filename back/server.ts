8fac52cd085a502b1045f65f77c02ca74bd9d896
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-02-09 16:09:44 +0100

    dev: added server

---
+++ back/server.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import session from "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    user?: { displayName: string };
+  }
+}
+
+const port = 3000;
+
+const app = express();
+
+app.use(
+  session({
+    name: "photobook.sid",
+    secret: "do not change this secret or all session will be reset...",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.json());
+
+app.get("/api/isConnected", (req, res) => {
+  if (req.session.user) {
+    res.json(req.session.user);
+    return;
+  }
+  res.status(401).end();
+});
+
+app.post("/api/connect", (req, res) => {
+  const credentials: { email: string; password: string } = req.body;
+  console.log("credentials: ", credentials);
+  if (credentials.email !== "jlg@jlg.com") {
+    res.status(401).end();
+    return;
+  }
+  req.session.user = { displayName: "Jean-Louis GUENEGO" };
+  res.json(req.session.user);
+});
+
+app.post("/api/disconnect", (req, res) => {
+  req.session.user = undefined;
+  res.status(200).end();
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
