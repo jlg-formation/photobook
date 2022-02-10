7105dd5b21c3e120da8a78b790e178d6ffe5f1d3
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-02-10 16:27:32 +0100

    dev: added newpost

--- front/PhotoBook/src/posts/NewPost.tsx
+++ front/PhotoBook/src/posts/NewPost.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
 import {useAppSelector} from '../redux/hooks';
 import {selectAuthentication} from '../redux/slices/authentication.slice';
 
 const NewPost = () => {
   const authentication = useAppSelector(selectAuthentication);
+  const [text, onChangeText] = useState('');
+
   return (
     <View style={styles.view}>
-      <Text style={styles.text}>
-        Hello {authentication.user?.displayName}, what's on your mind?
-      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={`Hello ${authentication.user?.displayName}, what's on your mind?`}
+      />
+      <Button title="Post" />
     </View>
   );
 };
@@ -18,10 +24,11 @@ const styles = StyleSheet.create({
   view: {
     backgroundColor: 'white',
     width: '95%',
-    height: 200,
+
     margin: 10,
   },
-  text: {
+  input: {
+    height: 200,
     fontFamily: 'sans-serif',
     fontStyle: 'italic',
   },
