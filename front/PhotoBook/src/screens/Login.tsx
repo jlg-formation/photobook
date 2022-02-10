e216915e1bd88d1a773b8ec50872320d754c9c12
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-02-10 15:50:37 +0100

    dev: navigate to Wall after Login

--- front/PhotoBook/src/screens/Login.tsx
+++ front/PhotoBook/src/screens/Login.tsx
@@ -31,7 +31,7 @@ const Login = ({navigation}: ScreenProps<'Login'>) => {
 
   useEffect(() => {
     if (authentication.user) {
-      navigation.navigate('Home');
+      navigation.navigate('Wall');
     }
   });
 
