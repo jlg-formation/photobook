aa8953fc1ec8cd01b084dffc5d5ce858d2e41390
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-02-13 17:57:43 +0100

    dev: added font-awesome-5 icons

--- front/PhotoBook/src/screens/Home.tsx
+++ front/PhotoBook/src/screens/Home.tsx
@@ -7,6 +7,8 @@ import Legal from './Legal';
 import Settings from './Settings';
 import Wall from './Wall';
 
+import {Icon} from 'react-native-elements';
+
 const Tab = createBottomTabNavigator();
 
 const Home = ({navigation}: ScreenProps<'Home'>) => {
@@ -18,9 +20,44 @@ const Home = ({navigation}: ScreenProps<'Home'>) => {
   }, [navigation, authentication]);
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
         headerShown: false,
-      }}>
+        tabBarIcon: ({focused, color, size}) => {
+          console.log('focused: ', focused, route.name);
+          const icon = {
+            name: 'home',
+          };
+
+          switch (route.name) {
+            case 'Wall':
+              icon.name = 'home';
+              if (focused) {
+                icon.name = 'cat';
+              }
+              break;
+            case 'Legal':
+              icon.name = 'balance-scale';
+              break;
+            case 'Settings':
+              icon.name = 'cog';
+              break;
+            default:
+          }
+
+          // You can return any component that you like here!
+          return (
+            <Icon
+              name={icon.name}
+              type="font-awesome-5"
+              size={size}
+              color={color}
+              tvParallaxProperties={undefined}
+            />
+          );
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
       <Tab.Screen name="Wall" component={Wall} />
       <Tab.Screen name="Legal" component={Legal} />
       <Tab.Screen name="Settings" component={Settings} />
